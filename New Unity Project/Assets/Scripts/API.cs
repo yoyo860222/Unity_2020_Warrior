bc8d4a1634bade869b95f03bf344bb7eb89bc795
yoyo860222 <75553513+yoyo860222@users.noreply.github.com>
2021-01-03 13:05:22 +0800

    場景切換工作中

---
+++ New Unity Project/Assets/Scripts/API.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class API : MonoBehaviour
+{
+  //修飾詞   類 別    欄位
+    public Transform traA;
+    public Transform tester;
+    public Camera cam;
+    public SpriteRenderer RenA;
+    public GameObject ObjA;
+
+    private void Start()
+    {
+        // 非靜態
+        // 取得
+        // 類別.屬性
+
+        print("座標:"+ traA.position);
+        print("顏色:"+ RenA.color);
+        
+
+
+        // 類別. 屬性
+        tester.position = new Vector3(0, 2, 0);
+        cam.backgroundColor = new Color(0.5f,0.2f,0.3f);
+        RenA.flipX = true;
+        ObjA.layer = 5;
+
+        print("圖層:" + ObjA.layer);
+
+
+    }
+
+    private void Update()
+    {
+        //使用方法
+
+        //欄位. 方法 (對,應,引,數)
+        tester.Rotate(0, 0, 10);
+        tester.Translate(0.1f, 0, 0, Space.World);
+        
+    }
+
+
+}
---
+++ New Unity Project/Assets/Scripts/Scenescontrol.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenescontrol : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("入場場景");
+    }
+
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    //離開遊戲
+    public void ExitGame()
+    {
+        //應用程式.離開()
+        Application.Quit();
+    }
+
+
+}
