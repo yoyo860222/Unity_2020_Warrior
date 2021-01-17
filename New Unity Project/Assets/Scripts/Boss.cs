3f5d483e5146371cdb16e0d1901e5d1dc5bf28b5
yoyo860222 <75553513+yoyo860222@users.noreply.github.com>
2021-01-17 17:06:27 +0800

    子彈腳本

---
+++ New Unity Project/Assets/Scripts/Boss.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+//第一次套用腳本時執行，幫物品增加一堆東西
+[RequireComponent(typeof(AudioSource),typeof(Rigidbody2D),typeof(CapsuleCollider2D))]
+public class Boss : MonoBehaviour
+{
+
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 10;
+
+    [Header("偵測範圍"), Range(0, 100)]
+    public float rangeAtk = 10;
+
+    [Header("攻擊力"), Range(0, 1000)]
+    public float Attack = 10;
+
+    [Header("血量"), Range(0, 5000)]
+    public float hp = 2500;
+
+    private Animator ani;
+    private AudioSource aud;
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
+        rig = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("受傷觸發");
+    }
+
+
+
+}
---
+++ New Unity Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    /// <summary>
+    /// 子彈攻擊力
+    /// </summary>
+    public float attack;
+
+
+    /// <summary>
+    /// 沒有勾選 On Tigger 的 碰撞事件
+    /// </summary>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //如果碰撞物件有Boss腳本
+        if (collision.gameObject.GetComponent<Boss>())
+        {
+
+            //對 Boss 呼叫 Damage(攻擊力)
+            collision.gameObject.GetComponent<Boss>().Damage(attack);
+
+        }
+        
+    }
+
+
+}
