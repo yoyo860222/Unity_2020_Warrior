4d2702f100acce9cd94b99006768e7280d464491
yoyo860222 <75553513+yoyo860222@users.noreply.github.com>
2021-01-31 16:14:12 +0800

    魔王觸發

---
+++ New Unity Project/Assets/Scripts/BossTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    [Header("Boss血條")]
+    public GameObject Bosshp;
+    [Header("Boss")]
+    public GameObject Bossbody;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name ==("Warrior"))
+        {
+            Bosshp.SetActive(true);
+            Bossbody.SetActive(true);
+        }
+    }
+
+}
